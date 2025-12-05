8a27644a45c7f4b686f3db9c6f344f1ef3922030
David Tonderski <dtonderski@gmail.com>
2025-12-05 15:50:17 +0100

    have gemini solve day1.2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"os"
 	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -17,9 +17,50 @@ func check(e error) {
 	}
 }
 
+func floorDiv(a, b int) int {
+	if a < 0 && a%b != 0 {
+		return a/b - 1
+	}
+	return a / b
+}
+
+func move_dial_and_check_password_increment(dial *int, password *int, input string, new_algo bool) {
+	abs_change, _ := strconv.Atoi(input[1:])
+
+	target := *dial
+	if input[0] == 'L' {
+		target -= abs_change
+	} else {
+		target += abs_change
+	}
+
+	if !new_algo {
+		*dial = target
+		if *dial%100 == 0 {
+			*password += 1
+		}
+		return
+	}
+
+	if target > *dial {
+		next_boundary := (floorDiv(*dial, 100) + 1) * 100
+		for next_boundary <= target {
+			*password++
+			next_boundary += 100
+		}
+	} else {
+		next_boundary := floorDiv(*dial-1, 100) * 100
+		for next_boundary >= target {
+			*password++
+			next_boundary -= 100
+		}
+	}
+
+	*dial = target
+}
 
 func main() {
-	file, err := os.Open(file) 
+	file, err := os.Open(file)
 	check(err)
 	defer file.Close()
 
@@ -29,20 +70,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		abs_change, err := strconv.Atoi(line[1:])
-		check(err)
-		if (line[0] == 'L') {
-			dial_pos -= abs_change
-		} else {
-			dial_pos += abs_change
-		}
-
-		if dial_pos%100 == 0 { 
-			password ++
-		}
+		move_dial_and_check_password_increment(&dial_pos, &password, line, true)
 	}
-	
+
 	fmt.Printf("password = %d\n", password)
 }
-
-

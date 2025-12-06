310f63a28076f842b0d03c841827e56f665162e1
David Tonderski <dtonderski@gmail.com>
2025-12-06 02:00:03 +0100

    feat: solve day3.2

--- 2025/day3/main.go
+++ 2025/day3/main.go
@@ -24,7 +24,7 @@ func timeTrack(start time.Time, name string) {
     log.Printf("%s took %s", name, elapsed)
 }
 
-func getLargestInt(line string) int {
+func getLargestIntOld(line string) int {
 	first, second := 0, 0
 	for _, c := range line {
 		num, err := strconv.Atoi(string(c))
@@ -37,6 +37,53 @@ func getLargestInt(line string) int {
 		}
 	}
 	return 10*first + second
+}
+
+func getLargestInt(line string, size int) int {
+	if (len(line) < size) {
+		panic("wtf")
+	}
+
+	
+	line_ints := make([]int, len(line), len(line))
+	arr := make([]int, size, size)
+
+	for i := 0; i < len(line); i++ {
+		num, err := strconv.Atoi(string(line[i])) 
+		check(err)
+		line_ints[i] = num
+		if (i < size) {
+			arr[i] = num
+		}
+	}
+
+	for i := size; i < len(line); i++ {
+		num := line_ints[i]
+
+		shifting := false
+		for j := 0; j < size-1; j++ {
+			if arr[j] < arr[j+1] {
+				shifting = true
+			}
+
+			if shifting {
+				arr[j] = arr[j+1]
+			}
+		}
+		if shifting || num > arr[size-1] {
+			arr[size-1] = num
+		}
+	}
+	
+	s := ""
+	for _, num := range arr {
+		s += strconv.Itoa(num)
+	}
+		
+	res, err := strconv.Atoi(s)
+	check(err)
+	return res
+
 }
 
 func main() {
@@ -52,7 +99,8 @@ func main() {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		largest := getLargestInt(line)
+		// largest := getLargestIntOld(line)
+		largest := getLargestInt(line, 12)
 		sum += largest
 	}
 	
